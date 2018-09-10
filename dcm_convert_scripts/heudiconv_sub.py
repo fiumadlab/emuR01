00f6a8a122039be2729e73a8bc7e4133e0e1d586
Adam Kimbler <akimbler@fiu.edu>
2018-09-10 11:00:39 -0400

    Debug

--- dcm_convert_scripts/heudiconv_sub.py
+++ dcm_convert_scripts/heudiconv_sub.py
@@ -14,7 +14,8 @@ for subject in SUBJECTS:
         -f /home/data/madlab/scripts/emuR01/dcm_convert_scripts/heuristic_emu_heudi.py \
         -o /scratch/madlab/dicoms/emuR01/".format(sess, subject)
         sp.Popen('sbatch -J heudiconv_{0}_{1} \
-                  -p investor -N 1 --mail-type=END,FAIL \
-                  --mail-user=akimbler@fiu.edu \
-                  --qos pq_madlab --wrap="{2}"'.format(subject, sess, heudi_cmd),
+                 -p investor -N 1 --mail-type=END,FAIL \
+                 -e ~/{0}_{1}_err \
+                 --mail-user=akimbler@fiu.edu \
+                 --qos pq_madlab --wrap="{2}"'.format(subject, sess, heudi_cmd),
                  shell=True)
