7b40c27c0269a1bd5d75b4feee2ffcea1978e3d8
Adam Kimbler <akimbler@fiu.edu>
2018-09-10 13:17:40 -0400

    Fixed bids format

--- dcm_convert_scripts/heuristic_emu_heudi.py
+++ dcm_convert_scripts/heuristic_emu_heudi.py
@@ -15,21 +15,18 @@ def infotodict(seqinfo):
     """
 
     rest = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_run-{item:02d}_bold')
-    study_r1 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-study_run-{item:02d}_bold')
-    study_r2 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-study_run-{item:02d}_bold')
-    test_r1 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-{item:02d}_bold')
-    test_r2 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-{item:02d}_bold')
-    test_r3 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-{item:02d}_bold')
-    fmstudy = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-func_dir-{dir}_fm_study-{item:02d}_epi')
-    fmtest = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-func_dir-{dir}_fm_test-{item:02d}_epi')
-    fmrest = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-func_dir-{dir}_fm_rest-{item:02d}_epi')
-    fmdwi = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-dwi_dir-{dir}_fm_dwi-{item:02d}_epi')
+    study_r1 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-study_run-01_bold')
+    study_r2 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-study_run-02_bold')
+    test_r1 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-01_bold')
+    test_r2 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-02_bold')
+    test_r3 = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-test_run-03_bold')
+    fmfunc = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-func_dir-{dir}_run-{item:02d}_epi')
+    fmdwi = create_key('sub-{subject}/{session}/fmap/sub-{subject}_{session}_acq-dwi_dir-{dir}_run-{item:02d}_epi')
     dwi = create_key('sub-{subject}/{session}/dwi/sub-{subject}_{session}_dwi-{item:02d}_dwi')
     t1 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_T1-{item:02d}_T1w')
     pd = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_PD')
     info = {rest: [], study_r1: [], study_r2: [], test_r1: [], test_r2: [],
-            test_r3: [], fmstudy: [], fmtest: [], fmrest: [],
-            fmdwi: [], dwi: [], t1: [], pd: []}
+            test_r3: [], fmfunc: [], fmdwi: [], dwi: [], t1: [], pd: []}
     for s in seqinfo:
         x, y, sl, nt = (s[6], s[7], s[8], s[9])
         print(s[12])
@@ -38,25 +35,23 @@ def infotodict(seqinfo):
         elif (nt == 238) and ('fMRI_Emotion_PA_Rest' in s[12]):
             info[rest].append(int(s[2]))
         elif (nt == 305) and ('fMRI_Emotion_PS_Study_1' in s[12]):
-            info[study_r1].append(s[2])
-        elif (nt == 303) and ('fMRI_Emotion_PS_Study_2' in s[12]):
-            info[study_r2].append(s[2])
+            info[study_r1] = [s[2]]
+        elif 'fMRI_Emotion_PS_Study_2' in s[12]:
+            info[study_r2] = [s[2]]
         elif (nt == 293) and ('fMRI_Emotion_PS_Test_1' in s[12]):
-            info[test_r1].append(s[2])
+            info[test_r1] = [s[2]]
         elif (nt == 293) and ('fMRI_Emotion_PS_Test_2' in s[12]):
-            info[test_r2].append(s[2])
+            info[test_r2] = [s[2]]
         elif (nt == 288) and ('fMRI_Emotion_PS_Test_3' in s[12]):
-            info[test_r3].append(s[2])
+            info[test_r3] = [s[2]]
         elif (sl > 1) and (nt == 103) and ('dMRI' in s[12]):
             info[dwi].append(s[2])
-        elif s[12] in ('fMRI_DistortionMap_AP_Rest', 'fMRI_DistortionMap_PA_Rest'):
-            info[fmrest].append({'item':s[2], 'dir':s[12][-7:-5]})
-        elif s[12] in ('fMRI_DistortionMap_AP_PS_STUDY', 'fMRI_DistortionMap_PA_PS_STUDY'):
-            info[fmstudy].append({'item':s[2], 'dir':s[12][-11:-9]})
-        elif s[12] in ('fMRI_DistortionMap_AP_Test', 'fMRI_DistortionMap_PA_PS_Test'):
-            info[fmtest].append({'item':s[2], 'dir':s[12][-10:-8]})
         elif s[12] in ('dMRI_DistortionMap_AP_dMRI', 'dMRI_DistortionMap_PA_dMRI'):
             info[fmdwi].append({'item':s[2], 'dir':s[12][-7:-5]})
+        elif 'DistortionMap_AP' in s[12]:
+            info[fmfunc].append({'item':s[2], 'dir':'AP'})
+        elif 'DistortionMap_PA' in s[12]:
+            info[fmfunc].append({'item':s[2], 'dir':'PA'})
         elif (sl == 30) and ('pd_tse_Cor_T2' in s[12]):
             info[pd].append(s[2])
         else:
